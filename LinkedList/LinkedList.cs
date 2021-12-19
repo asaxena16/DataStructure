1d3bae94c73e6ee6b8d56f21eac924eefb95bc61
aadesh.k.saxena@gmail.com <aadesh.k.saxena@gmail.com>
2021-12-19 09:22:44 +0530

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSctructure.LinkedList
+{
+    public class LinkedList
+    {
+        public class Node
+        {
+            internal object data;
+            internal Node nextElement;
+
+            public Node()
+            {
+                nextElement = null;
+            }
+        };
+        private Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal Node FindMiddle()
+        {
+            Node currentNode = head;
+            Node fastNode = head;
+
+            while (currentNode != null)
+            {
+                currentNode = currentNode.nextElement;
+                fastNode = fastNode.nextElement.nextElement;
+                if (fastNode == null || fastNode.nextElement == null)
+                {
+                    break;
+                }
+            }
+            return currentNode;
+        }
+
+        public void InsertAtHead(int value)
+        {
+            Node newnode = new Node
+            {
+                data = value,
+                nextElement = head
+            };
+            head = newnode;
+            Console.WriteLine(value + " Inserted !");
+        }
+        public void InsertAtEnd(int value)
+        {
+            if (IsEmpty())
+            {
+                InsertAtHead(value);
+            }
+            else
+            {
+                Node newnode = new Node { data = value };
+                Node traversingNode = head;
+                while (traversingNode.nextElement != null)
+                {
+                    traversingNode = traversingNode.nextElement;
+                }
+                traversingNode.nextElement = newnode;
+                Console.Write(value + " Inserted!    \n");
+            }
+        }
+
+        public bool DeleteAtHead()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return false;
+            }
+
+            head = head.nextElement;
+            return true;
+        }
+        internal bool Delete(int value)
+        {
+            bool deleted = false;
+            if (IsEmpty())
+            {
+                return deleted;
+            }
+            Node currentNode = head;
+            if ((int)currentNode.data == value)
+            {
+                DeleteAtHead();
+                return true;
+            }
+            Node previousNode = currentNode;
+            currentNode = currentNode.nextElement;
+            while (currentNode != null)
+            {
+                if (value == (int)currentNode.data)
+                {
+                    previousNode.nextElement = currentNode.nextElement;
+                    deleted = true;
+                    break;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.nextElement;
+            }
+            if (deleted)
+            {
+                Console.WriteLine("Deleted !!!!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Doesnt exist");
+                return false;
+            }
+        }
+
+        internal bool Find(int value)
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+            Node temp = head;
+            while (temp != null)
+            {
+                if ((int)temp.data == value)
+                {
+                    return true;
+                }
+                temp = temp.nextElement;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace DataSctructure
+{
+    class Program
+    {
+        static void Main()
+        {
+            //BasicOperationonLinkedList();
+
+            //FindOneByNthofLinkedList();
+            MIDDLE_OF_THE_LINKED_LIST();
+        }
+
+        private static void MIDDLE_OF_THE_LINKED_LIST()
+        {
+            #region PopulateLinkedList
+            LinkedList.LinkedList list = new LinkedList.LinkedList();
+            for (int i = 0; i < 18; i++)
+            {
+                list.InsertAtEnd(i);
+            }
+            #endregion
+
+            LinkedList.LinkedList.Node node = list.FindMiddle();
+            Console.WriteLine("Middle of Linked List is - " + node.data);
+        }
+
+        private static void FindOneByNthofLinkedList()
+        {
+            #region PopulateLinkedList
+            LinkedList.LinkedList list = new LinkedList.LinkedList();
+            for (int i = 0; i < 20; i++)
+            {
+                list.InsertAtEnd(i);
+            }
+            #endregion
+
+            //FIND 1/2 OF LL
+
+        }
+
+        private static void BasicOperationonLinkedList()
+        {
+            LinkedList.LinkedList list = new LinkedList.LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    list.InsertAtHead(i);
+                }
+
+                list.InsertAtEnd(i);
+            }
+
+            Console.WriteLine("Value is Present - " + list.Find(5));
+            list.DeleteAtHead();
+            list.Delete(5);
+        }
+    }
+}
